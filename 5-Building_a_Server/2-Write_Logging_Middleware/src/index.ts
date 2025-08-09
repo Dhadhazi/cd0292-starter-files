4ffebd68f3deb3b0bbe2c99e522c5e6973d4ff5c
Daniel Hadhazi <dhadhazi@gmail.com>
2025-08-09 14:51:46 +0200

    port update

--- 5-Building_a_Server/2-Write_Logging_Middleware/src/index.ts
+++ 5-Building_a_Server/2-Write_Logging_Middleware/src/index.ts
@@ -1,9 +1,7 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const port = 3000;
-
-
+const port = 3001;
 
 // start the Express server
 app.listen(port, () => {
